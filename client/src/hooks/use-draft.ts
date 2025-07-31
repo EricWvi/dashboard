e404a28281107e02c5bd256dc1ce25a0aad0f933
Eric Wang <eric88wang@qq.com>
2025-07-31 16:38:28 +0800

    v0.2.0

--- client/src/hooks/use-draft.ts
+++ client/src/hooks/use-draft.ts
@@ -1,5 +1,5 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
-import { apiRequest, queryClient } from "@/lib/queryClient";
+import { useQuery } from "@tanstack/react-query";
+import { apiRequest } from "@/lib/queryClient";
 
 export type Draft = {
   id: number;
