8a531c6cfbdedb2d2da12a71a5aa9b36bd50824f
Eric Wang <eric88wang@qq.com>
2026-06-14 11:37:35 +0800

    refactor: move assets

--- apps/web/client/src/main.tsx
+++ apps/web/client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index-journal.css";
-import App from "./JournalApp.tsx";
+import "./index.css";
+import App from "./App.tsx";
 import { initMediaServerBaseUrl } from "@/lib/utils";
 
 async function bootstrap() {
